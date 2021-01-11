680fa14f1b4cb44a7d74fc67cac6b6c1541f3099
IpunkDKK <ipunka37@gmail.com>
2021-01-11 21:13:09 +0700

    fix beberapa komponen pada integrasi.cpp

--- integrasi.cpp
+++ integrasi.cpp
@@ -6,7 +6,8 @@ int main(int argc, char const *argv[])
 {
 	double xt[100] , x[100] , fx[100] , fxb[100] , selisih , hasil1 , hasil2, total ,eksak;
 	int banyak , pilih;
-	cout << "menu pilihan kaidah \n1.trapesium\n2.simson 1/3\n3.simson 3/8\n4.titik tengah\nMasukan pilihan : ";
+	cout << "metode pias = p\nmetode interpolasi = i\n\n";
+	cout << "menu pilihan kaidah \n1.trapesium(i)\n2.simson 1/3(i)\n3.simson 3/8(i)\n4.titik tengah(p)\n5.segi empat(p)\n6.trapeesium(p)\nMasukan pilihan : ";
 	cin >> pilih;
 	cout << "berapa banyak data : ";
 	cin >> banyak;
@@ -34,13 +35,19 @@ int main(int argc, char const *argv[])
 	// mencari fx baru
 	for (int i =0; i<banyak; i++){
 		fxb[i] = exp(xt[i]);
-		cout << fxb[i] <<endl;
+	}
+	// membuat table
+	cout << "|i\t|" <<"x\t|" <<"xt \t|" << "fx\t\t|" << "fxb\t|\n";
+	cout << "----------------------------------------------------\n"; 
+	for (int i = 0; i<banyak; i++){
+		cout <<"|" <<i+1 <<"\t|" << x[i] << "\t|" <<xt[i] <<"\t|" <<fx[i] <<"\t|" << fxb[i] <<"|"<< endl;
 	}
 
 	//rumus
 
 	switch(pilih){
 		case 1 :
+		//kaidah trapesium
 		hasil1 = selisih / 2;
 			for (int i = 0; i<banyak; i++){
 				if (i == 0){
@@ -53,6 +60,7 @@ int main(int argc, char const *argv[])
 			}
 			break;
 		case 2 :
+		//kaidah simson 1/3
 		hasil1 = selisih / 3;
 			for (int i = 0 ; i < banyak ; i++){
 				if (i == 0){
@@ -67,29 +75,58 @@ int main(int argc, char const *argv[])
 			}
 			break;
 		case 3 : 
+			// kaidah simson 3/8
 			hasil1 = ((3*selisih) / 8);
 			for (int i = 0; i <banyak; i++){
 				if (i == 0){
-					hasil2 += fxb[i];
+					hasil2 += fx[i];
 				}else if (i == banyak-1){
-					hasil2 += fxb[i];
+					hasil2 += fx[i];
 				}else if (i % 3 == 0){
-					hasil2 += 2 * fxb[i];
-				}else{
-					hasil2 += 3 * fxb[i];
+					hasil2 += (2 * fx[i]);
+				}else if (i % 3 != 0){
+					hasil2 += (3 * fx[i]);
 				}
 			}
 			break;
 		case 4 :
+		//kaidah titik tengah
 		hasil1 = selisih;
 			for(int i = 0; i < banyak; i++){
 				if (i == banyak-1){
-					cout << "hallo" <<endl;
 					break;
 				}else{
 					hasil2 += fxb[i];
 				}
 			}
+			break;
+		case 5 : 
+		//kaidah segiempat
+		hasil1 = selisih / 2 ;
+			for (int i = 0; i < banyak; i++){
+				if (i == 0){
+					hasil2 += fx[i];
+				}else if(i == banyak-1){
+					hasil2 += fx[i];
+				}else{
+					hasil2 += 2*fx[i];
+				}
+			}
+			break;
+		case 6 :
+		//kaidah trapesium
+		hasil1 = selisih / 2 ;
+			for (int i = 0; i < banyak; i++){
+				if (i == 0){
+					hasil2 += fx[i];
+				}else if(i == banyak-1){
+					hasil2 += fx[i];
+				}else{
+					hasil2 += 2*fx[i];
+				}
+			}
+			break;
+
 	}
 	total = hasil1 * hasil2;
 	eksak = abs(exp(x[0]) - exp(x[banyak-1])) ;
